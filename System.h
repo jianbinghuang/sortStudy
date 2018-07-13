dbf612b92be3d663288e3a415151f3fdeadec5cb
Huang, Jianbing <jianbing.huang@siemens.com>
2018-07-13 10:29:38 -0500

    Add more changes

--- System.h
+++ System.h
@@ -7,14 +7,4 @@
 
 #define Demand(test, msg) if(!test) throw std::exception(msg);
 
-// rotate the array between l and r so that
-// a[l] = a[r], and a[k+1] = a[k] for l<=k<r
-template<typename T> inline void rotate(T* a, int32_t l, int32_t r)
-{
-	assert(r>=l);
-	T t = a[r];
-	for(int32_t i=r-1; i>=l; --i) a[i+1] = a[i];
-    a[l] = t;
-}
-
 #endif
--- Utility.h
+++ Utility.h
@@ -10,4 +10,14 @@ template<typename T> void swap(T* a, int32_t l, int32_t r)
 	a[r] = temp;
 }
 
+// rotate the array between l and r so that
+// a[l] = a[r], and a[k+1] = a[k] for l<=k<r
+template<typename T> inline void rotate(T* a, int32_t l, int32_t r)
+{
+    assert(r >= l);
+    T t = a[r];
+    for (int32_t i = r - 1; i >= l; --i) a[i + 1] = a[i];
+    a[l] = t;
+}
+
 #endif
